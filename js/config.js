5cb77f40288d9d47d0722d729a48214d5585285a
SeunB <125329091+balogsun@users.noreply.github.com>
2024-09-20 17:49:18 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_M2sAYgzKy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5d78b8k80opt2dnhuh4umqruka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'ca-central-1_8pW3W5ILn', 
+        userPoolClientId: '82f624f930ehven0b2uea7rlk', 
+        region: 'ca-central-1' 
     },
     api: {
         invokeUrl: 'https://6m3jri1cj3.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
